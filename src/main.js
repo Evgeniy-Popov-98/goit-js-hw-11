94d885d1551912ea480a58302bd2c949523b44b4
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-02-03 12:21:11 +0200

    add-gallery

--- src/main.js
+++ src/main.js
@@ -4,41 +4,6 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 /*=============================================================*/
-const img = {
-  total: 4692,
-  totalHits: 500,
-  hits: [
-    {
-      id: 195893,
-      pageURL: 'https://pixabay.com/en/blossom-bloom-flower-195893/',
-      type: 'photo',
-      tags: 'blossom, bloom, flower',
-      previewURL:
-        'https://cdn.pixabay.com/photo/2013/10/15/09/12/flower-195893_150.jpg',
-      previewWidth: 150,
-      previewHeight: 84,
-      webformatURL: 'https://pixabay.com/get/35bbf209e13e39d2_640.jpg',
-      webformatWidth: 640,
-      webformatHeight: 360,
-      largeImageURL: 'https://pixabay.com/get/ed6a99fd0a76647_1280.jpg',
-      fullHDURL: 'https://pixabay.com/get/ed6a9369fd0a76647_1920.jpg',
-      imageURL: 'https://pixabay.com/get/ed6a9364a9fd0a76647.jpg',
-      imageWidth: 4000,
-      imageHeight: 2250,
-      imageSize: 4731420,
-      views: 7671,
-      downloads: 6439,
-      likes: 5,
-      comments: 2,
-      user_id: 48777,
-      user: 'Josch13',
-      userImageURL:
-        'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg',
-    },
-  ],
-};
-
-/*====================== FORM ====================*/
 
 const form = document.querySelector('.js-form');
 const input = document.querySelector('.js-input');
@@ -46,8 +11,6 @@ const buttom = document.querySelector('.js-button');
 const gallery = document.querySelector('.js-gallery');
 
 form.addEventListener('submit', creatGallery);
-
-// let url =  'https://pixabay.com/api/?key=42169950-0e8cca4ed1d3fcef898dec13a&q=cat&image_type=photo&orientation=horizontal&safesearch=true';
 
 function creatGallery(event) {
   event.preventDefault();
@@ -75,64 +38,25 @@ function getImages(infoSearch) {
         throw myError;
       }
     })
-    .then(res => renderImages(res))
+    .then(resolt => renderImages(resolt.hits))
     .catch(err => console.log(err.message));
 }
 
-function imageTemplate({
-  largeImageURL,
-  previewURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) {
+function imageTemplate(img) {
   return `
-		  <li class="gallery-item">
-		  <a class="gallery-link" href=${largeImageURL}>
-			<img class="gallery-image" src=${previewURL} alt="${tags}" />
-			<div class="container-text"><h3 class="titel-text">Likes</h3><p class="text">${likes}</p></div>
-			<div class="container-text"><h3 class="titel-text">Views</h3><p class="text">${views}</p></div>
-			<div class="container-text"><h3 class="titel-text">Comments</h3><p class="text">${comments}</p></div>
-			<div class="container-text"><h3 class="titel-text">Dowloads</h3><p class="text">${downloads}</p></div>
-		  </a>
-		</li>
-		`;
-}
-
-function imagesTemplate(images) {
-  return images.map(imageTemplate).join('');
+			  <li class="gallery-item">
+			  <a class="gallery-link" href=${img.largeImageURL}>
+				<img class="gallery-image" src=${img.previewURL} alt="${img.tags}" />
+				<div class="container-text"><h3 class="titel-text">Likes</h3><p class="text">${img.likes}</p></div>
+				<div class="container-text"><h3 class="titel-text">Views</h3><p class="text">${img.views}</p></div>
+				<div class="container-text"><h3 class="titel-text">Comments</h3><p class="text">${img.comments}</p></div>
+				<div class="container-text"><h3 class="titel-text">Dowloads</h3><p class="text">${img.downloads}</p></div>
+			  </a>
+			</li>
+			`;
 }
 
 function renderImages(images) {
-  console.log(images);
-  const markup = imagesTemplate(images);
-  console.log(markup);
+  const markup = images.map(imageTemplate).join('');
   gallery.innerHTML = markup;
 }
-/*================= GALLERY ===============*/
-// const gallery = document.querySelector('.js-gallery');
-
-// function imagesTemplate(item) {
-//   return `
-// 		  <li class="gallery-item">
-// 		  <a class="gallery-link" href=${item.original}>
-// 			<img class="gallery-image" src=${item.preview} alt="${item.description}" />
-// 		  </a>
-// 		</li>
-// 		`;
-// }
-
-// function renderGallery() {
-//   const markup = images.map(imagesTemplate).join('');
-//   gallery.innerHTML = markup;
-// }
-
-// renderGallery();
-
-// let galleryModalWindow = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionPosition: 'bottom',
-//   captionDelay: 250,
-// });
